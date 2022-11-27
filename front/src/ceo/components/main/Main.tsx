8836cbf208a0f79458b2fc921fabc4d6881488fa
임동민 <ldm96216@gmail.com>
2022-11-27 23:03:50 +0900

    Temp

--- front/src/ceo/components/main/Main.tsx
+++ front/src/ceo/components/main/Main.tsx
@@ -4,42 +4,40 @@ import styled from "styled-components";
 import Complete from "./Complete";
 import Delivery from "./Delivery";
 import Wait from "./Wait";
-import { EventSourcePolyfill } from "event-source-polyfill";
+import { EventSourcePolyfill, NativeEventSource } from "event-source-polyfill";
 
 function Main() {
   // const [reviewInfo, setReviewInfo] = useState();
+  const EventSource = EventSourcePolyfill || NativeEventSource;
 
   useEffect(() => {
     // EventSource 로 Server Sent Event 를 호출하는 부분
-    const eventSource = new EventSourcePolyfill(
-      "http://localhost:8081/api/ceo/sse/connect",
-      {
-        headers: {
-          Authorization: `Bearer ${sessionStorage.getItem("jwt")}`,
-        },
-      }
-    );
+    const eventSource = new EventSource("http://175.45.208.84:8081/api/ceo/sse/connect", {
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem("jwt")}`,
+      },
+    });
     console.log(eventSource);
 
-    eventSource.addEventListener("message", (e) => {
-      const newElement = document.createElement("li");
-
-      newElement.textContent = `message: ${e.data}`;
-    });
+    eventSource.onmessage = (event: any) => {
+      const data = JSON.parse(event.data);
+      console.log(data.message);
+    };
 
     eventSource.onopen = function () {
       // 연결 됐을 때
       console.log("connected");
     };
-    eventSource.onerror = function (error) {
+    eventSource.onerror = function (error: any) {
       // 에러 났을 때
       console.log("error" + JSON.stringify(error));
     };
-    eventSource.onmessage = function (stream) {
+    eventSource.onmessage = function (stream: any) {
       // 메세지 받았을 때
       const parsedData = JSON.parse(stream.data);
       console.log(parsedData);
     };
+    return () => eventSource.close();
   }, []);
 
   return (
