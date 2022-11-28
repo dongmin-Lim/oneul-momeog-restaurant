b696487abe7671d9d999a10516641d1cfd3cc715
임동민 <ldm96216@gmail.com>
2022-11-28 10:57:36 +0900

    메인페이지 백엔드 연동

--- front/src/ceo/components/main/Finish.tsx
+++ front/src/ceo/components/main/Finish.tsx
@@ -1,15 +1,11 @@
 import styled from "styled-components";
 import Card from "./Card";
 
-function Complete() {
-  return (
-    <Div>
-      접수완료 <Card status="complete" />
-    </Div>
-  );
+function Finish({ receiveInfo }: any) {
+  return <Div>배달중</Div>;
 }
 
 const Div = styled.div`
   /* border: 1px solid black; */
 `;
-export default Complete;
+export default Finish;
